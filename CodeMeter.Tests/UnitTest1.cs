91a6d37e68b6b0cabc5a7af2088e445b8036302f
jbstinson <johnbstinson@gmail.com>
2026-05-25 13:05:19 -0500

    chore: scaffold WPF app and test projects

---
+++ CodeMeter.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace CodeMeter.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ CodeMeter/App.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace CodeMeter;
+
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        // Wiring added in Task 11
+    }
+}
